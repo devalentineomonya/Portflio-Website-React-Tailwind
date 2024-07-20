8a5c2dbe2798369f157ab92964cd74d9dd3d141c
Valentine Omonya <valomosh254@gmail.com>
2024-07-20 18:59:50 +0300

    Modified the experienceCard files

--- src/components/common/2ExperienceCard/ExperienceCard.jsx
+++ src/components/common/2ExperienceCard/ExperienceCard.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import "./experiencecard.css";
 const ExperienceCard = () => {
   return (
-    <div>
-      
+    <div className="experience-card">
+      <div className="experience-card-container">
+        <div className="experience-card-content">
+          <div className="experience-card-icon"></div>
+          <div className="experience-role">
+            <p>2023 - 2021</p>
+            <h3>Junior Frontend Engineer</h3>
+          </div>
+          <div className="company-info">
+            <h3>Quick Pointe </h3>
+            <div>
+              <span>NodeJS</span> <span>ReactJS</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ExperienceCard
+export default ExperienceCard;

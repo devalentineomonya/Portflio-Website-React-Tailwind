9414f448b22cd17c3be96c30e30c66cfb8d59563
Valentine Omonya <valomosh254@gmail.com>
2024-07-20 21:11:15 +0300

    Modified experience file

--- src/components/Home/2Experience/Experience.jsx
+++ src/components/Home/2Experience/Experience.jsx
@@ -1,15 +1,56 @@
 import React from "react";
 import ExperienceCard from "../../common/2ExperienceCard/ExperienceCard";
-import { CarouselProvider, Slide } from "pure-react-carousel";
+import {
+  ButtonBack,
+  ButtonNext,
+  CarouselProvider,
+  Slide,
+  Slider,
+} from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
-import "./experience.css"
+import "./experience.css";
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 const Experience = () => {
   return (
     <section className="experience-section">
       <div className="section-title">My Experience</div>
-      <div>
-        <ExperienceCard />
-      </div>
+      <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={125}
+        totalSlides={8}
+        visibleSlides={5}
+        className="carousel-provider"
+      >
+        <Slider>
+          <Slide index={0}>
+            <ExperienceCard />
+          </Slide>
+          <Slide index={1}>
+            <ExperienceCard />
+          </Slide>
+          <Slide index={3}>
+            <ExperienceCard />
+          </Slide>
+          <Slide index={4}>
+            <ExperienceCard />
+          </Slide>
+          <Slide index={5}>
+            <ExperienceCard />
+          </Slide>
+          <Slide index={6}>
+            <ExperienceCard />
+          </Slide>
+          <Slide index={7}>
+            <ExperienceCard />
+          </Slide>
+        </Slider>
+        <ButtonBack className="button-back">
+          <BiChevronLeft size={44} />
+        </ButtonBack>
+        <ButtonNext className="button-next">
+          <BiChevronRight size={44} />
+        </ButtonNext>
+      </CarouselProvider>
     </section>
   );
 };

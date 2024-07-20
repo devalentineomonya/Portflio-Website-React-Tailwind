f6349224110bb00cae7eca5f5e9d0e70630b198a
Valentine Omonya <valomosh254@gmail.com>
2024-07-20 21:12:03 +0300

    Modified soft skills components

--- src/components/Home/3SoftSkills/SoftSkills.jsx
+++ src/components/Home/3SoftSkills/SoftSkills.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import "./softskills.css";
 const SoftSkills = () => {
+  const softSkills = ["Communication", "Problem-Solving", "Team Leader", "", "5", "6", "7", "8", "9", "10"];
   return (
-    <div>
-      <div className="soft-skill-container">
-        <div className="soft-skill-icon"></div>
-        <div className="soft-skill-title"></div>
-      </div>
+    <div className="soft-skill-container">
+      {softSkills.map((skill, i) => (
+        <div className="soft-skill-card" key={i}>
+          <div className="soft-skill-icon"></div>
+          <div className="soft-skill-title">{skill}</div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default SoftSkills
+export default SoftSkills;

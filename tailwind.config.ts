6191d758ec14412aa5d1e37021ba13a4ca38798c
Valentine Omonya <valomosh254@gmail.com>
2025-02-26 03:50:48 +0300

    Updated Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import tailwindAnimate from "tailwindcss-animate"
 export default {
     darkMode: ["class"],
     content: [
-    "./features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./screens/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],

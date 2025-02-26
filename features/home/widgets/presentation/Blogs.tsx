367b95077f9d325649521a965080ec64b3db5411
Valentine Omonya <valomosh254@gmail.com>
2025-02-26 03:50:42 +0300

    Removed Blogs widget

--- features/home/widgets/presentation/Blogs.tsx
+++
@@ -1,8 +0,0 @@
-import { TabsContent } from "@/components/ui/tabs";
-import React from "react";
-
-const Blogs = () => {
-  return <TabsContent value="blogs">Change your password here.</TabsContent>;
-};
-
-export default Blogs;
